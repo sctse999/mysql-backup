b3945a6372471cdaa2383500d35c21226b45e587
sctse999 <jonathan@tsez.net>
2017-04-18 07:26:43 +0800

    Update run.sh
    Add ${MYSQL_DB} to restore script

--- run.sh
+++ run.sh
@@ -52,7 +52,7 @@ rm -f /restore.sh
 cat <<EOF >> /restore.sh
 #!/bin/bash
 echo "=> Restore database from \$1"
-if mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASS} < \$1 ;then
+if mysql -h${MYSQL_HOST} -P${MYSQL_PORT} -u${MYSQL_USER} -p${MYSQL_PASS} ${MYSQL_DB} < \$1 ;then
     echo "   Restore succeeded"
 else
     echo "   Restore failed"
